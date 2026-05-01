e6f7874dc3be7b7bc21c4f827240ef35a6c43a2c
Smit Vaghasiya <146537968+SmitVgithub@users.noreply.github.com>
2026-05-01 18:32:30 +0530

    🔍 Recon Brain: Update components/Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import { Menu } from 'semantic-ui-react';
 import Cookies from 'js-cookie';
+import DOMPurify from 'dompurify';
+
+// Helper function to safely get and sanitize cookie values
+const getSanitizedEmail = () => {
+	const email = Cookies.get('company_email') || Cookies.get('voter_email') || '';
+	// Basic email validation regex
+	const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+	if (email && emailRegex.test(email)) {
+		return DOMPurify.sanitize(email);
+	}
+	return '';
+};
 
 export default props => {
 	return (
@@ -11,7 +23,12 @@ export default props => {
 				}
 			`}</style>
 			<Menu secondary style={{ maxHeight: '50px' }}>
-				<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css" />
+				<link
+					rel="stylesheet"
+					href="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css"
+					integrity="sha384-JKIDqM48bt14NZpzl9v0AP36v/j5xT+N0rZvL5HvNMjW/bDPSDTzM0gL+PjLov7y"
+					crossOrigin="anonymous"
+				/>
 				<Menu.Item
 					name="BlockVotes"
 					style={{ verticalAlign: 'middle', fontSize: '40px', paddingLeft: '42%', paddingTop: '4%' }}
@@ -19,7 +36,7 @@ export default props => {
 				<Menu.Menu position="right">
 					<Menu.Item icon="user" />
 					<Menu.Item style={{ paddingRight: '10px' }}>
-						{Cookies.get('company_email') || Cookies.get('voter_email')}
+						{getSanitizedEmail()}
 					</Menu.Item>
 				</Menu.Menu>
 			</Menu>
