87affff16486d4e1c30d93295e38a1639e800c45
Smit Vaghasiya <146537968+SmitVgithub@users.noreply.github.com>
2026-05-16 14:46:15 +0530

    🔍 Recon Brain: Update config/database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,23 @@
 //Set up mongoose connection
 const mongoose = require('mongoose');
-const mongoDB = 'mongodb://localhost/BlockVotes';
-mongoose.connect(mongoDB,{ useNewUrlParser: true });
+
+// Load connection string from environment variable
+const mongoDB = process.env.MONGODB_URI;
+
+if (!mongoDB) {
+  console.error('MONGODB_URI environment variable is not set');
+  process.exit(1);
+}
+
+mongoose.connect(mongoDB)
+  .then(() => {
+    console.log('MongoDB connected successfully');
+  })
+  .catch((err) => {
+    console.error('MongoDB connection error:', err.message);
+    process.exit(1);
+  });
+
 mongoose.Promise = global.Promise;
+
 module.exports = mongoose;
